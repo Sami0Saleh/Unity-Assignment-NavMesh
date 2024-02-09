29ba272575f21f5f0d0ce25b25f50a86975c8cfc
RonCohen47 <138518157+RonCohen47@users.noreply.github.com>
2024-02-09 14:11:10 +0200

    Added Ilan's Package

---
+++ Unity Assignment NavMesh/Assets/Scripts/IlanCharacterController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IlanCharacterController : MonoBehaviour
+{
+    private const int mudAreaID = 3;
+
+    [SerializeField] private NavMeshAgent navMeshAgent;
+    [SerializeField] private Transform[] pathWaypoints;
+
+    private int currentWaypointIndex = 0;
+    private void Start()
+    {
+        navMeshAgent.SetDestination(pathWaypoints[currentWaypointIndex].position);
+    }
+
+    private void Update()
+    {
+        if (!navMeshAgent.isStopped && navMeshAgent.remainingDistance <= 0.1f)
+        {
+            currentWaypointIndex++;
+            if (currentWaypointIndex >= pathWaypoints.Length)
+                currentWaypointIndex = 0;
+            navMeshAgent.SetDestination(pathWaypoints[currentWaypointIndex].position);
+        }
+    }
+}
